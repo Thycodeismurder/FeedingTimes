23a99f9964e125f2be0112641ba8c783b114b4f1
Thycodeismurder <ville.loytola@outlook.com>
2023-11-09 20:42:38 +0200

    form template done

--- client/FeedingTimes/src/app/action-form-view/form/form.component.ts
+++ client/FeedingTimes/src/app/action-form-view/form/form.component.ts
@@ -7,17 +7,28 @@ import { FormBuilder, Validators } from '@angular/forms';
   styleUrls: ['./form.component.scss'],
 })
 export class FormComponent implements OnInit {
-  actionForm = this.fb.group({
-    Mother: ['', Validators.required],
-    Father: ['', Validators.required],
-    Action: ['', Validators.required],
-    Time: ['', Validators.required],
-    Quantity: ['', Validators.required],
-  });
+  actionForm = this.fb.group(
+    {
+      Mother: ['', Validators.required],
+      Father: ['', Validators.required],
+      Action: ['', Validators.required],
+      Time: ['', Validators.required],
+      Quantity: ['', Validators.required],
+    },
+    { updateOn: 'submit' }
+  );
   constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {}
   onSubmit() {
-    console.log(this.actionForm.value);
+    console.log(this.actionForm.valid);
+    if (!this.actionForm.valid) {
+      this.actionForm.markAsDirty({ onlySelf: true });
+      console.log(this.actionForm.value);
+    } else {
+      const errors = this.actionForm.errors;
+      console.log(errors);
+      console.log(this.actionForm.value);
+    }
   }
 }
