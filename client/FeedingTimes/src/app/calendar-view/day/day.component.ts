0347aeb4343b49e95f427c449e814ae08f6696c4
Thycodeismurder <ville.loytola@outlook.com>
2024-02-16 09:04:12 +0200

    code cleaning

--- client/FeedingTimes/src/app/calendar-view/day/day.component.ts
+++ client/FeedingTimes/src/app/calendar-view/day/day.component.ts
@@ -1,9 +1,7 @@
 import {
   Component,
   Input,
-  OnChanges,
   OnInit,
-  SimpleChanges,
 } from '@angular/core';
 import { Activity } from 'src/services/ActivityType';
 import { Feeding, UserEvent } from 'src/services/UserType';
@@ -15,11 +13,9 @@ import { TimedEvent } from 'src/services/timedEvent';
   templateUrl: './day.component.html',
   styleUrls: ['./day.component.scss'],
 })
-export class DayComponent implements OnInit, OnChanges {
+export class DayComponent implements OnInit {
   @Input() activities: (Activity | null | undefined)[] | undefined;
-  activitiesFeeding: Feeding[] | undefined;
-  activitiesUserEvent: UserEvent[] | undefined;
-  transformEventData: any;
+  transformEventData: TransformEventDataPipe;
   constructor() {
     this.transformEventData =  new TransformEventDataPipe();
   }
@@ -27,30 +23,4 @@ export class DayComponent implements OnInit, OnChanges {
    return this.transformEventData.transform(data)
   }
   ngOnInit(): void {}
-  ngOnChanges(changes: SimpleChanges) {
-    if (changes['activities']) {
-      this.activitiesFeeding = this.activities?.reduce(
-        (newArr: Feeding[], activity) => {
-          if (activity) {
-            if ('quantity' in activity) {
-              newArr.push(activity);
-            }
-          }
-          return newArr;
-        },
-        []
-      );
-      this.activitiesUserEvent = this.activities?.reduce(
-        (newArr: UserEvent[], activity) => {
-          if (activity) {
-            if ('description' in activity) {
-              newArr.push(activity);
-            }
-          }
-          return newArr;
-        },
-        []
-      );
-    }
-  }
 }
